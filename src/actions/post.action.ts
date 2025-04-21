c63ef7b259f6d0ab7e8c093e2023e59cdbbd5087
TeddyLe68 <teddyle2003@gmail.com>
2025-04-21 23:20:22 +0700

    post action + header post

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -31,6 +31,7 @@ export async function getPosts() {
       include: {
         author: {
           select: {
+            id: true,
             name: true,
             username: true,
             image: true,
@@ -68,5 +69,140 @@ export async function getPosts() {
   } catch (error) {
     console.error("Error getting posts:", error);
     throw new Error("Error getting posts");
+  }
+}
+
+export async function toggleLike(postId: string) {
+  try {
+    const userId = await getDbUserId();
+    if (!userId) return;
+
+    // check if like exists
+    const existingLike = await prisma.like.findUnique({
+      where: {
+        userId_postId: {
+          postId,
+          userId,
+        },
+      },
+    });
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+    if (!post) throw new Error("Post not found");
+
+    if (existingLike) {
+      // unlike post
+      await prisma.like.delete({
+        where: {
+          userId_postId: {
+            postId,
+            userId,
+          },
+        },
+      });
+    } else {
+      // like post and notification (only if liking someone else's post)
+      await prisma.$transaction([
+        prisma.like.create({
+          data: {
+            postId,
+            userId,
+          },
+        }),
+        ...(post.authorId !== userId
+          ? [
+              prisma.notification.create({
+                data: {
+                  type: "LIKE",
+                  userId: post.authorId, // post author
+                  creatorId: userId, // person who liked the post,
+                  postId,
+                },
+              }),
+            ]
+          : []),
+      ]);
+    }
+
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    console.error("Error liking post:", error);
+    return { success: false, error: "Filed to the toggle like" };
+  }
+}
+
+export async function createComment(postId: string, content: string) {
+  try {
+    const userId = await getDbUserId();
+    if (!userId) return;
+
+    if (!content) throw new Error("Comment content is required");
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) throw new Error("Post not found");
+
+    // Create comment and notification in a transaction
+    const [comment] = await prisma.$transaction(async (tx) => {
+      // create first commment
+      const newComment = await tx.comment.create({
+        data: {
+          content,
+          postId,
+          authorId: userId,
+        },
+      });
+      // create notification if the comment is on someone else's post
+      if (post.authorId !== userId) {
+        await tx.notification.create({
+          data: {
+            type: "COMMENT",
+            userId: post.authorId, // post author
+            creatorId: userId, // person who commented
+            postId,
+            commentId: newComment.id,
+          },
+        });
+      }
+      return [newComment];
+    });
+
+    revalidatePath(`/posts/${postId}`);
+    return { success: true, comment };
+  } catch (error) {
+    console.error("Error creating comment:", error);
+    return { success: false, error: "Filed to create comment" };
+  }
+}
+
+export async function deletePost(postId: string) {
+  try {
+    const userId = await getDbUserId();
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: { authorId: true },
+    });
+
+    if (!post) throw new Error("Post not found");
+    if (post.authorId !== userId)
+      throw new Error("You are not the author of this post");
+
+    await prisma.post.delete({
+      where: { id: postId },
+    });
+
+    revalidatePath("/");
+    return { success: true };
+  } catch (error) {
+    console.error("Error deleting post:", error);
+    return { success: false, error: "Filed to delete post" };
   }
 }
