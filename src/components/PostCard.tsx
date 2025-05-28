b3aa987da7c6eb41f4c2f01e94cc261f9a565cd3
TeddyLe68 <teddyle2003@gmail.com>
2025-05-28 22:44:57 +0700

    like+comment section

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,7 +6,7 @@ import {
   getPosts,
   toggleLike,
 } from "@/actions/post.action";
-import { useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { Card, CardContent } from "./ui/card";
@@ -14,6 +14,15 @@ import Link from "next/link";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { formatDistanceToNow } from "date-fns";
 import DeleteAlertDialog from "./DeleteAlertDialog";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import {
+  HeartIcon,
+  LogInIcon,
+  MessageCircleIcon,
+  SendIcon,
+} from "lucide-react";
+import { Textarea } from "./ui/textarea";
 
 type Posts = Awaited<ReturnType<typeof getPosts>>;
 type Post = Posts[number];
@@ -27,6 +36,7 @@ function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
     post.likes.some((like) => like.userId === dbUserId)
   );
   const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+  const [showComments, setShowComments] = useState(false);
 
   const handleLike = async () => {
     if (isLiking) return;
@@ -120,6 +130,147 @@ function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
               </p>
             </div>
           </div>
+          {/* Post image */}
+          {post.image && (
+            <div className="rounded-lg overflow-hidden">
+              <img
+                src={post.image}
+                alt="Post contebt"
+                className="w-full h-auto object-cover"
+              />
+            </div>
+          )}
+          {/* Like and comment button */}
+          <div className="flex items-center pt-2 space-x-4">
+            {user ? (
+              <Button
+                variant="ghost"
+                size="sm"
+                className={`text-muted-foreground gap-2 ${
+                  hasLiked
+                    ? "text-red-500 hover:text-red-600"
+                    : "hover:text-red-500"
+                }`}
+                onClick={handleLike}
+              >
+                {hasLiked ? (
+                  <HeartIcon className="size-5 fill-current" />
+                ) : (
+                  <HeartIcon className="size-5" />
+                )}
+                <span>{optimisticLikes}</span>
+              </Button>
+            ) : (
+              <SignInButton mode="modal">
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  className="text-muted-foreground gap-2"
+                >
+                  <HeartIcon className="size-5" />
+                </Button>
+              </SignInButton>
+            )}
+
+            {user ? (
+              <Button
+                className="text-muted-foreground gap-2
+                hover:text-blue-500"
+                variant="ghost"
+                size="sm"
+                onClick={() => setShowComments((prev) => !prev)}
+              >
+                <MessageCircleIcon
+                  className={`size-5 ${
+                    showComments ? "fill-blue-500 text-blue-500" : ""
+                  }`}
+                  onClick={() => setShowComments((prev) => !prev)}
+                />
+                <span>{post.comments.length}</span>
+              </Button>
+            ) : (
+              <SignInButton mode="modal">
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  className="text-muted-foreground gap-2"
+                >
+                  <MessageCircleIcon className="size-5" />
+                </Button>
+              </SignInButton>
+            )}
+          </div>
+          {/* comment section */}
+          {showComments && (
+            <div className="space-y-4 p-4 border-t">
+              <div className="space-y-4">
+                {post.comments.map((comment) => (
+                  <div key={comment.id} className="flex space-x-3">
+                    <Avatar className="size-8 flex-shrink-0">
+                      <AvatarImage
+                        src={comment.author.image ?? "/avatar.png"}
+                      />
+                    </Avatar>
+                    <div className="flex-1 min-w-0">
+                      <div className="flex flex-wrap item-center gap-x-2 gap-y-1">
+                        <span className="font-medium text-sm">
+                          {comment.author.name}
+                        </span>
+                        <span className="text-sm text-muted-foreground">
+                          @{comment.author.username}
+                        </span>
+                        <span className="text-sm text-muted-foreground">.</span>
+                        <span className="text-sm text-muted-foreground">
+                          {formatDistanceToNow(new Date(comment.createdAt))} ago
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              {user ? (
+                <div className="flex space-x-3">
+                  <Avatar>
+                    <AvatarImage src={user?.imageUrl || "/avatar.png"} />
+                  </Avatar>
+                  <div className="flex-1 ">
+                    <Textarea
+                      placeholder="Write a comment..."
+                      value={newComment}
+                      onChange={(e) => setNewComment(e.target.value)}
+                      className="min-h-[80px] resize-none"
+                    />
+                    <div className="flex justify-end mt-2">
+                      <Button
+                        onClick={handleAddComment}
+                        className="flex items-center gap-2"
+                        size="sm"
+                        disabled={!newComment.trim() || isCommenting}
+                      >
+                        {isCommenting ? (
+                          "Posting..."
+                        ) : (
+                          <>
+                            <SendIcon className="size-4" />
+                            Comment
+                          </>
+                        )}
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex justify-center p-4 border rounded-lg bg-muted/50">
+                  <SignInButton mode="modal">
+                    <Button className="gap-2 cursor-pointer" variant="outline">
+                      <LogInIcon className="size-4" />
+                      Sign in to comment
+                    </Button>
+                  </SignInButton>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </CardContent>
     </Card>
